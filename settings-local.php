b7c1776a2f3d5dd4f12a04adff64c0244f1e62ec
emil <emil@htmlbox.net>
2016-11-14 18:09:05 +0200

    Add settings-local.php

---
+++ settings-local.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+	/*
+	| SMTP configuration
+	|
+	| Use https://mailtrap.io/ for testing. 
+	|
+	*/
+	'smtp_config' => array(
+		"host"       => '',
+		"port"       => '',
+		"username"   => '',
+		"password"   => '',
+		"encryption" => false, // "ssl", "tls" or false for plain-text
+	),
+);
